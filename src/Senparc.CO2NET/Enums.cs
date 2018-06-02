8cdcee8945359191d9a5832a38d7ed2c9f6131aa
Jeffrey Su <zsu@senparc.com>
2018-06-02 20:50:29 +0800

    add Enums

---
+++ src/Senparc.CO2NET/Enums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.CO2NET
+{
+
+    /// <summary>
+    /// 缓存类型
+    /// </summary>
+    public enum CacheType
+    {
+        /// <summary>
+        /// 本地运行时缓存（单机）
+        /// </summary>
+        Local,
+        /// <summary>
+        /// Redis缓存（支持分布式）
+        /// </summary>
+        Redis,
+        /// <summary>
+        /// Memcached（支持分布式）
+        /// </summary>
+        Memcached
+    }
+
+}
