7439d0b79fa93fc70f2e941e0a35429fe6feccc8
Jeffrey Su <zsu@senparc.com>
2018-06-02 20:51:09 +0800

    add SenparcTraceHelper

---
+++ src/Senparc.CO2NET/Trace/SenparcTraceHelper/SenparcTraceHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.CO2NET.Trace
+{
+    public class SenparcTraceHelper
+    {
+         
+    }
+}
---
+++ src/Senparc.CO2NET/Trace/SenparcTraceHelper/SenparcTraceItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.CO2NET.Trace
+{
+    public class SenparcTraceItem
+    {
+        public string Title { get; set; }
+        public DateTime DateTime { get; set; }
+
+    }
+}
