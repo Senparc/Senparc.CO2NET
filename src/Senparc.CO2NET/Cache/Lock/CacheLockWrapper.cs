56dc8b55df1dd56cf5b3b4cc05d3dc97f2dea51e
Jeffrey Su <zsu@senparc.com>
2018-06-02 19:26:23 +0800

    有时你的生活或种种会像在这个版本的代码一样一团乱麻，但只要你想清楚自己要什么、不要什么，坚持做下去，不断精进，迟早有一天它会成为master中的骄傲。

---
+++ src/Senparc.CO2NET/Cache/Lock/CacheLockWrapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.CO2NET.Cache
+{
+    public class CacheLockWrapper : IDisposable
+    {
+        private string _resourceName;
+        private IBaseCacheStrategy _containerCacheStragegy;
+        private int retryCount;
+        private TimeSpan _retryDelay;
+
+        public bool LockSuccessful { get; set; }
+
+        public CacheLockWrapper(IBaseCacheStrategy containerCacheStragegy, string resourceName, string key)
+            : this(containerCacheStragegy, resourceName, key, 0, new TimeSpan())
+        {
+
+        }
+
+        public CacheLockWrapper(IBaseCacheStrategy containerCacheStragegy, string resourceName, string key, int retryCount, TimeSpan retryDelay)
+        {
+            _containerCacheStragegy = containerCacheStragegy;
+            _resourceName = resourceName + key;/*加上Key可以针对某个AppId加锁*/
+
+            if (retryCount != 0 && retryDelay.Ticks != 0)
+            {
+                LockSuccessful = _containerCacheStragegy.Lock(_resourceName, retryCount, retryDelay);
+            }
+            else
+            {
+                LockSuccessful = _containerCacheStragegy.Lock(_resourceName);
+            }
+        }
+
+        public void Dispose()
+        {
+            _containerCacheStragegy.UnLock(_resourceName);
+        }
+    }
+}
