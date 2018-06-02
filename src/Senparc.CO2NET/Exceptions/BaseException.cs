e91be503f261b1b6de725a67408ad31519ff3ca3
Jeffrey Su <zsu@senparc.com>
2018-06-02 19:04:49 +0800

    添加 BaseException

---
+++ src/Senparc.CO2NET/Exceptions/BaseException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.CO2NET.Exceptions
+{
+    /// <summary>
+    /// 异常基类
+    /// </summary>
+#if NET35 || NET40 || NET45
+    public class BaseException :  ApplicationException
+#else
+    public class BaseException : Exception
+#endif
+    {
+        public BaseException(string message, bool logged = false)
+            : this(message, null, logged)
+        {
+        }
+
+        /// <summary>
+        /// WeixinException
+        /// </summary>
+        /// <param name="message">异常消息</param>
+        /// <param name="inner">内部异常信息</param>
+        /// <param name="logged">是否已经使用WeixinTrace记录日志，如果没有，WeixinException会进行概要记录</param>
+        public BaseException(string message, Exception inner, bool logged = false)
+            : base(message, inner)
+        {
+            if (!logged)
+            {
+                //WeixinTrace.Log(string.Format("WeixinException（{0}）：{1}", this.GetType().Name, message));
+                //WeixinTrace.WeixinExceptionLog(this);
+            }
+        }
+    }
+}
