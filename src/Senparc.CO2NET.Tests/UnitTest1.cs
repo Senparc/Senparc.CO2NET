bb06ab81b628fac1da950ff07284a7cd984bc0c0
Jeffrey Su <zsu@senparc.com>
2018-06-05 23:48:16 +0800

    添加 Senparc.CO2NET.Tests

---
+++ src/Senparc.CO2NET.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Senparc.CO2NET.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
