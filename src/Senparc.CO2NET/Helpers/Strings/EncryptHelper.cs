74938c1c6a41d6266ae9520df1a16da3389eccc7
Jeffrey Su <zsu@senparc.com>
2018-06-08 22:25:36 +0800

    update Encrypt.cs

--- src/Senparc.CO2NET/Helpers/Strings/EncryptHelper.cs
+++ src/Senparc.CO2NET/Helpers/Strings/EncryptHelper.cs
@@ -89,6 +89,36 @@ namespace Senparc.CO2NET.Helpers
             //}
         }
 
+        #region 弃用算法
+
+
+//        /// <summary>
+//        /// 签名算法
+//        /// </summary>
+//        /// <param name="str"></param>
+//        /// <returns></returns>
+//        public static string GetSha1(string str)
+//        {
+//            //建立SHA1对象
+//#if NET45
+//            SHA1 sha = new SHA1CryptoServiceProvider();
+//#else
+//            SHA1 sha = SHA1.Create();
+//#endif
+
+//            //将mystr转换成byte[] 
+//            ASCIIEncoding enc = new ASCIIEncoding();
+//            byte[] dataToHash = enc.GetBytes(str);
+//            //Hash运算
+//            byte[] dataHashed = sha.ComputeHash(dataToHash);
+//            //将运算结果转换成string
+//            string hash = BitConverter.ToString(dataHashed).Replace("-", "");
+//            return hash;
+//        }
+
+        #endregion
+
+
         /// <summary>
         /// HMAC SHA256 加密
         /// </summary>
