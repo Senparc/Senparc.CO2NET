da999f2e2ac11a96daae3973d72daf258af79551
Jeffrey Su <zsu@senparc.com>
2018-06-09 12:52:46 +0800

    创建 扩展领域缓存策略 IExtensionDomainCacheStrategy

---
+++ src/Senparc.CO2NET/Cache/CacheStrategy/IExtensionDomainCacheStrategy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.CO2NET.Cache
+{
+    /// <summary>
+    /// 扩展领域缓存策略
+    /// </summary>
+    public interface IExtensionDomainCacheStrategy
+    {
+        /// <summary>
+        /// 唯一名称（建议使用GUID）
+        /// </summary>
+        string IdentityName { get; set; }
+        string DomainName { get; set; }
+    }
+}
