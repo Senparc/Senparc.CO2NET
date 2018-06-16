73e3807ddc96e375aff6c6e9a70a6afb836fc2a9
Jeffrey Su <zsu@senparc.com>
2018-06-16 15:19:36 +0800

    添加 CacheStrategyDomainWarehouseTests

---
+++ src/Senparc.CO2NET.Tests/Cache/CacheStrategy/BaseCacheStrategyTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Senparc.CO2NET.Tests.Cache.CacheStrategy
+{
+    //抽象类，请看Cache.Lock中的测试
+
+    //[TestClass]
+    //public class BaseCacheStrategyTests
+    //{
+    //    [TestMethod]
+    //    public void TestMethod1()
+    //    {
+    //    }
+    //}
+}
