bf6ebcb128131fc7bc86756d28397603f642c1c9
Jeffrey Su <zsu@senparc.com>
2018-06-16 14:43:33 +0800

    update GetFinalKey()

--- src/Senparc.CO2NET/Cache/CacheStrategy/BaseCacheStrategy.cs
+++ src/Senparc.CO2NET/Cache/CacheStrategy/BaseCacheStrategy.cs
@@ -45,10 +45,10 @@ namespace Senparc.CO2NET.Cache
     /// </summary>
     public abstract class BaseCacheStrategy : IBaseCacheLock
     {
-        /// <summary>
-        /// 默认下级命名空间
-        /// </summary>
-        public virtual string ChildNamespace { get; set; }
+        ///// <summary>
+        ///// 默认下级命名空间
+        ///// </summary>
+        //public virtual string ChildNamespace { get; set; }
 
         /// <summary>
         /// 获取拼装后的FinalKey
@@ -58,7 +58,7 @@ namespace Senparc.CO2NET.Cache
         /// <returns></returns>
         public virtual string GetFinalKey(string key, bool isFullKey = false)
         {
-            return isFullKey ? key : String.Format("Senparc:{0}:{1}:{2}", ChildNamespace, Config.DefaultCacheNamespace, key);
+            return isFullKey ? key : $"Senparc:{Config.DefaultCacheNamespace}:{key}";
         }
 
         /// <summary>
