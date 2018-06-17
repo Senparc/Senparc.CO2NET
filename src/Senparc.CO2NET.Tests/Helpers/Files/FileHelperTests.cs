f3bf1a1cee61178fd50d4befc3d4a110b75c2bca
Jeffrey Su <zsu@senparc.com>
2018-06-18 00:22:20 +0800

    FileHelperTests 测试全通过

--- src/Senparc.CO2NET.Tests/Helpers/Files/FileHelperTests.cs
+++ src/Senparc.CO2NET.Tests/Helpers/Files/FileHelperTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Senparc.CO2NET.Helpers;
 
@@ -16,6 +17,20 @@ namespace Senparc.CO2NET.Tests.Helpers
             Assert.IsTrue(stream.Length > 0);
             Console.WriteLine(stream.Length);
             Assert.AreEqual(117025, stream.Length);//只对当前Logo.jpg有效
+        }
+
+        [TestMethod]
+        public void DownLoadFileFromUrlTest()
+        {
+            var url = "https://sdk.weixin.senparc.com//images/v2/ewm_01.png";
+            var savePath = $"..\\..\\..\\TestEntities\\download-{DateTime.Now.ToString("yyyyMMdd-HHmmss")}.jpg";
+
+            FileHelper.DownLoadFileFromUrl(url, savePath);
+
+            Assert.IsTrue(File.Exists(savePath));
+
+            //删除文件
+            File.Delete(savePath);
         }
     }
 }
