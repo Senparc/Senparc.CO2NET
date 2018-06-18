8018dfa60f0f32be1bedee418266180b6ae0f4e3
Jeffrey Su <zsu@senparc.com>
2018-06-18 17:45:14 +0800

    add SenparcMessageQueueTests.TestAll()

--- src/Senparc.CO2NET.Tests/MessageQueue/SenparcMessageQueueTests.cs
+++ src/Senparc.CO2NET.Tests/MessageQueue/SenparcMessageQueueTests.cs
@@ -57,5 +57,31 @@ namespace Senparc.CO2NET.Tests.MessageQueue
 
             Console.WriteLine("队列处理完毕，当前队列数量：" + smq.GetCount());
         }
+
+        [TestMethod]
+        public void TestAll()
+        {
+            var mq = new SenparcMessageQueue();
+            var count = mq.GetCount();
+            var key = DateTime.Now.Ticks.ToString();
+
+            //Test Add()
+            var item = mq.Add(key, () => Console.WriteLine("测试SenparcMessageQueue写入Key=A"));
+            Assert.AreEqual(count + 1, mq.GetCount());
+            //var hashCode = item.GetHashCode();
+
+            //Test GetCurrentKey()
+            var currentKey = mq.GetCurrentKey();
+            Assert.AreEqual(key, currentKey);
+
+            //Test GetItem
+            var currentItem = mq.GetItem(currentKey);
+            Assert.AreEqual(currentItem.Key, item.Key);
+            Assert.AreEqual(currentItem.AddTime, item.AddTime);
+
+            //Test Remove
+            mq.Remove(key);
+            Assert.AreEqual(count, mq.GetCount());
+        }
     }
 }
