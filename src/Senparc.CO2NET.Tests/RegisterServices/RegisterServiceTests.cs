f30c43b5e307048505b6f4069164e34b1dea3364
Jeffrey Su <zsu@senparc.com>
2018-06-18 17:26:54 +0800

    add RegisterServiceTests 测试通过

---
+++ src/Senparc.CO2NET.Tests/RegisterServices/RegisterServiceTests.cs
@@ -0,0 +1,32 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Senparc.CO2NET.RegisterServices;
+using Microsoft.Extensions.Options;
+using Moq;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace Senparc.CO2NET.Tests.RegisterServices
+{
+    [TestClass]
+    public class RegisterServiceTests
+    {
+        [TestMethod]
+        public void RegisterServiceTest()
+        {
+            var isDebug = true;
+            Senparc.CO2NET.Config.IsDebug = !isDebug;//故意换成相反值
+
+            var mockEnv = new Mock<IHostingEnvironment>();
+            mockEnv.Setup(z => z.ContentRootPath).Returns(() =>Path.GetFullPath("..\\..\\..\\"));
+            RegisterService.Start(mockEnv.Object, isDebug);
+
+            Console.WriteLine(Senparc.CO2NET.Config.RootDictionaryPath);
+            Assert.IsTrue(Senparc.CO2NET.Config.RootDictionaryPath.Length > 0);
+            Assert.AreEqual(isDebug, Senparc.CO2NET.Config.IsDebug);
+
+            //测试根目录正确性，测试查找文件
+            Assert.IsTrue(File.Exists(Path.Combine(Senparc.CO2NET.Config.RootDictionaryPath, "TestEntities", "Logo.jpg")));
+        }
+    }
+}
---
+++ src/Senparc.CO2NET.Tests/Startup.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Senparc.CO2NET.Tests
+{
+    public static class Startup
+    {
+
+    }
+}
