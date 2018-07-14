e22e4424cc3bcd0d05db8c947af11af80201d09f
Jeffrey Su <zsu@senparc.com>
2018-07-14 21:36:48 +0800

    添加.net core sample

---
+++ Sample/Senparc.CO2NET.Sample.netcore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Senparc.CO2NET.Sample.netcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
