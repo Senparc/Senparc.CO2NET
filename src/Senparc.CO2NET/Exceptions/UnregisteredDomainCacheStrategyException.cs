da124afb2137532bf5913ff1b04330b43699c14f
Jeffrey Su <zsu@senparc.com>
2018-07-21 14:23:25 +0800

    v0.2.1 “当前扩展缓存策略没有进行注册”异常信息提供更加详细的缓存类型信息

--- src/Senparc.CO2NET/Exceptions/UnregisteredDomainCacheStrategyException.cs
+++ src/Senparc.CO2NET/Exceptions/UnregisteredDomainCacheStrategyException.cs
@@ -4,8 +4,11 @@
     文件名：UnregisteredDomainCacheStrategyException.cs
     文件功能描述：领域缓存未注册异常
 
-
     创建标识：Senparc - 20180707
+
+    修改标识：Senparc - 20180721
+    修改描述：v0.1.1 修复 RegisterService.Start() 的 isDebug 设置始终为 true 的问题
+
 
 ----------------------------------------------------------------*/
 
@@ -28,10 +31,10 @@ namespace Senparc.CO2NET.Exceptions
         /// <param name="domainCacheStrategyType"></param>
         /// <param name="objectCacheStrategyType"></param>
         public UnregisteredDomainCacheStrategyException(Type domainCacheStrategyType, Type objectCacheStrategyType)
-            : base("当前扩展缓存策略没有进行注册", true)
+            : base($"当前扩展缓存策略没有进行注册：{domainCacheStrategyType.ToString()}，{objectCacheStrategyType.ToString()}", true)
         {
             Trace.SenparcTrace.SendCustomLog("当前扩展缓存策略没有进行注册",
-                $"当前扩展缓存策略没有进行注册，CacheStrategyDomain：{domainCacheStrategyType}，IBaseObjectCacheStrategy：{objectCacheStrategyType}");
+                $"当前扩展缓存策略没有进行注册，CacheStrategyDomain：{domainCacheStrategyType.ToString()}，IBaseObjectCacheStrategy：{objectCacheStrategyType.ToString()}");
         }
     }
 }
