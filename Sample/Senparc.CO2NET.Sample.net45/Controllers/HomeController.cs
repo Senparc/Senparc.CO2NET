92dabcfb89583fec08ec63d30db20449d431a8c0
Jeffrey Su <zsu@senparc.com>
2018-07-23 23:16:12 +0800

    完善Demo备注

--- Sample/Senparc.CO2NET.Sample.net45/Controllers/HomeController.cs
+++ Sample/Senparc.CO2NET.Sample.net45/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Senparc.CO2NET.Cache;
+using Senparc.CO2NET.Cache.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,9 @@ namespace Senparc.CO2NET.Sample.net45.Controllers
     {
         public ActionResult Index()
         {
+            //如需立即改用某种缓存策略，只需要这样做（注意：是全局立即生效）：
+            //CacheStrategyFactory.RegisterObjectCacheStrategy(() => RedisObjectCacheStrategy.Instance);
+
             var cache = CacheStrategyFactory.GetObjectCacheStrategyInstance();
             var count = cache.Get<int>("IndexTest");
             count++;
