5b03b105b4388b2af865a8409841a5eaf1fd4a6a
zhao365845726 <MartyZane@hotmail.com>
2018-10-31 18:11:52 +0800

    优化日期类

--- src/Senparc.Common.SDK/DateTime/DateTimeHelper.cs
+++ src/Senparc.Common.SDK/DateTime/DateTimeHelper.cs
@@ -52,6 +52,11 @@ namespace Senparc.Common.SDK
             return dt.ToString("yyyy-MM-dd");
         }
 
+        /// <summary>
+        /// 获取一年中的第几周
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
         public static string GetNumberWeekDay(DateTime dt)
         {
             int y = dt.Year;
@@ -70,6 +75,13 @@ namespace Senparc.Common.SDK
             return val.ToString();
         }
 
+        /// <summary>
+        /// 获取星期几
+        /// </summary>
+        /// <param name="y"></param>
+        /// <param name="m"></param>
+        /// <param name="d"></param>
+        /// <returns></returns>
         public string GetChineseWeekDay(int y, int m, int d)
         {
             string[] weekstr = { "日", "一", "二", "三", "四", "五", "六" };
@@ -526,6 +538,58 @@ namespace Senparc.Common.SDK
             return strDate;
         }
 
+        #endregion
+
+        #region 时间与时间戳互转
+        /// <summary>
+        /// DateTime转换为JavaScript时间戳
+        /// </summary>
+        /// <returns></returns>
+        public static long ConvertToJavaScriptUnix(global::System.DateTime? time = null)
+        {
+            if (time == null)
+            {
+                time = global::System.DateTime.Now;
+            }
+            var startTime = TimeZone.CurrentTimeZone.ToLocalTime(new global::System.DateTime(1970, 1, 1)); // 当地时区
+            return (long)(time.Value - startTime).TotalMilliseconds; // 相差毫秒数
+        }
+
+        /// <summary>
+        /// JavaScript时间戳转换为DateTime
+        /// </summary>
+        /// <param name="jsTimeStamp">JavaScript时间戳</param>
+        /// <returns></returns>
+        public static global::System.DateTime JavaScriptUnixConvertToDateTime(long jsTimeStamp)
+        {
+            var startTime = TimeZone.CurrentTimeZone.ToLocalTime(new global::System.DateTime(1970, 1, 1)); // 当地时区
+            return startTime.AddMilliseconds(jsTimeStamp);
+        }
+
+        /// <summary>
+        /// DateTime转换为时间戳
+        /// </summary>
+        /// <returns></returns>
+        public static long ConvertToUnix(global::System.DateTime? time = null)
+        {
+            if (time == null)
+            {
+                time = global::System.DateTime.Now;
+            }
+            var startTime = TimeZone.CurrentTimeZone.ToLocalTime(new global::System.DateTime(1970, 1, 1)); // 当地时区
+            return (long)(time.Value - startTime).TotalSeconds; // 相差秒数
+        }
+
+        /// <summary>
+        /// 时间戳转换为DateTime
+        /// </summary>
+        /// <param name="unixTimeStamp">时间戳</param>
+        /// <returns></returns>
+        public static global::System.DateTime UnixConvertToDateTime(long unixTimeStamp)
+        {
+            var startTime = TimeZone.CurrentTimeZone.ToLocalTime(new global::System.DateTime(1970, 1, 1)); // 当地时区
+            return startTime.AddSeconds(unixTimeStamp);
+        }
         #endregion
     }
 }
