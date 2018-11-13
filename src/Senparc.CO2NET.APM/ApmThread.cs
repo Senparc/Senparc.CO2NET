4f874e8002f683460af04aea130027caac47bf8c
Jeffrey Su <zsu@senparc.com>
2018-11-13 22:10:44 +0800

    添加 Senparc.CO2NET.APM

---
+++ src/Senparc.CO2NET.APM/ApmThread.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Senparc.CO2NET.APM
+{
+    /// <summary>
+    /// APM 线程
+    /// </summary>
+    public class ApmThread
+    {
+
+    }
+}
---
+++ src/Senparc.CO2NET.APM/DataCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Senparc.CO2NET.APM
+{
+    public class DataCollection
+    {
+        const string  CACHE_NAMESPACE= "SENPARC_APM";
+
+
+    }
+}
