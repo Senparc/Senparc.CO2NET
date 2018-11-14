ae77c2b0437d6c611b029dbb787b726d3892ac97
Jeffrey Su <zsu@senparc.com>
2018-11-14 15:40:29 +0800

    modify ApmThread

--- src/Senparc.CO2NET.APM/ApmThread.cs
+++ src/Senparc.CO2NET.APM/ApmThread.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace Senparc.CO2NET.APM
@@ -9,15 +10,33 @@ namespace Senparc.CO2NET.APM
     public class ApmThread
     {
         private DateTime LastRecordTime = DateTime.MinValue;
+
+        /// <summary>
+        /// 确保已经到达下一分钟
+        /// </summary>
+        /// <param name="recordTime"></param>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        private bool IsNextMinute(DateTime lastTime, DateTime dateTime)
+        {
+            return lastTime.Year < dateTime.Year ||
+                   lastTime.Month < dateTime.Month ||
+                   lastTime.Day < dateTime.Day ||
+                   lastTime.Hour < dateTime.Hour ||
+                   lastTime.Minute < dateTime.Minute;
+        }
+
         public void Run()
         {
             while (true)
             {
-                //if ((SystemTime.Now- LastRecordTime))
+                if (IsNextMinute(LastRecordTime, SystemTime.Now))
                 {
+                    //进行多余数据的清理
 
+
+                    LastRecordTime = SystemTime.Now;
                 }
-
 
                 Thread.Sleep(1000 * 10);//间隔1分钟以内
             }
