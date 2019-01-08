6e7c7bb90f4edc19021b793b3e2a32e953a0205a
Jeffrey Su <zsu@senparc.com>
2019-01-08 21:15:09 +0800

    添加Console示例项目

---
+++ Sample/Senparc.CO2NET.Sample.Consoles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Senparc.CO2NET.Sample.Consoles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
