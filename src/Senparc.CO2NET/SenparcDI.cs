cd2bd8835a8ca9fdfd6bf65c8a80896682558c31
Jeffrey Su <zsu@senparc.com>
2019-01-18 14:33:02 +0800

    v0.5.2 添加 SenparcDI.GetRequiredService() 方法，提供线程内独立 ServiceProvider 实例

--- src/Senparc.CO2NET/SenparcDI.cs
+++ src/Senparc.CO2NET/SenparcDI.cs
@@ -14,10 +14,14 @@
     修改标识：pengweiqhca - 20180802
     修改描述：v0.2.8 添加 SenparcDI.GetIServiceProvider() 方法，以支持其他依赖注入框架
 
+    修改标识：pengweiqhca - 20190118
+    修改描述：v0.5.2 添加 SenparcDI.GetRequiredService() 方法，提供线程内独立 ServiceProvider 实例
+
 ----------------------------------------------------------------*/
 
 #if NETSTANDARD2_0
 using System;
+using System.Threading;
 using Microsoft.Extensions.DependencyInjection;
 using Senparc.CO2NET.Cache;
 
@@ -29,6 +33,8 @@ namespace Senparc.CO2NET
     public static class SenparcDI
     {
         private static ServiceProvider _globalServiceProvider;
+
+        public const string SENPARC_DI_THREAD_SERVICE_PROVIDER = "___SenparcDIThreadServiceProvider";
 
         /// <summary>
         /// 全局 ServiceCollection
@@ -43,7 +49,6 @@ namespace Senparc.CO2NET
         {
             return GlobalServiceCollection;
         }
-
 
         /// <summary>
         /// 已过期，请使用 GlobalIServiceProvider
@@ -60,11 +65,24 @@ namespace Senparc.CO2NET
         }
 
         private static object _globalIServiceProviderLock = new object();
+        private static object _threadIServiceProviderLock = new object();
 
         /// <summary>
         /// 全局 IServiceCollection 对象
         /// </summary>
         public static IServiceProvider GlobalIServiceProvider { get; set; }
+
+        /// <summary>
+        /// 线程内的 ServiceProvider
+        /// </summary>
+        public static IServiceProvider ThreadIServiceProvider
+        {
+            get
+            {
+                var threadServiceProvider = Thread.GetData(Thread.GetNamedDataSlot(SENPARC_DI_THREAD_SERVICE_PROVIDER)) as IServiceProvider;
+                return threadServiceProvider;
+            }
+        }
 
         /// <summary>
         /// 获取 ServiceProvider
@@ -84,34 +102,68 @@ namespace Senparc.CO2NET
         /// <summary>
         /// 获取 ServiceProvider
         /// </summary>
+        /// <param name="useGlobalServiceProvider">是否使用全局唯一 ServiceProvider 对象，默认为 false，即使用线程内唯一 ServiceProvider 对象</param>
         /// <returns></returns>
-        public static IServiceProvider GetIServiceProvider()
+        public static IServiceProvider GetIServiceProvider(bool useGlobalServiceProvider = false)
         {
-            if (GlobalIServiceProvider == null)
+            if (useGlobalServiceProvider)
             {
-                //加锁确保唯一
-                lock (_globalIServiceProviderLock)
+                if (GlobalIServiceProvider == null)
                 {
-                    if (GlobalIServiceProvider == null)
+                    //加锁确保唯一
+                    lock (_globalIServiceProviderLock)
                     {
-                        //注意：BuildServiceProvider() 方法每次会生成不同的 ServiceProvider 对象！
-                        GlobalIServiceProvider = GetServiceCollection().BuildServiceProvider();
+                        if (GlobalIServiceProvider == null)
+                        {
+                            //注意：BuildServiceProvider() 方法每次会生成不同的 ServiceProvider 对象！
+                            GlobalIServiceProvider = GetServiceCollection().BuildServiceProvider();
+                        }
                     }
                 }
-
+                return GlobalIServiceProvider;
             }
-            return GlobalIServiceProvider;
+            else
+            {
+                if (ThreadIServiceProvider == null)
+                {
+                    //加锁确保唯一
+                    lock (_threadIServiceProviderLock)
+                    {
+                        if (ThreadIServiceProvider == null)
+                        {
+                            //注意：BuildServiceProvider() 方法每次会生成不同的 ServiceProvider 对象！
+                            //GlobalIServiceProvider = GetServiceCollection().BuildServiceProvider();
+                            Thread.SetData(Thread.GetNamedDataSlot(SENPARC_DI_THREAD_SERVICE_PROVIDER), GetServiceCollection().BuildServiceProvider());
+                        }
+                    }
+                }
+                return ThreadIServiceProvider;
+            }
         }
 
 
         /// <summary>
         /// 使用 .net core 默认的 DI 方法获得实例
+        /// <para>如果未注册，返回 null</para>
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
-        public static T GetService<T>()
+        /// <param name="useGlobalServiceProvider">是否使用全局唯一 ServiceProvider 对象，默认为 false，即使用线程内唯一 ServiceProvider 对象</param>
+        public static T GetService<T>(bool useGlobalServiceProvider = false)
         {
-            return GetIServiceProvider().GetService<T>();
+            return GetIServiceProvider(useGlobalServiceProvider).GetService<T>();
+        }
+
+        /// <summary>
+        /// 使用 .net core 默认的 DI 方法获得实例（推荐）
+        /// <para>如果未注册，抛出异常 </para>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <param name="useGlobalServiceProvider">是否使用全局唯一 ServiceProvider 对象，默认为 false，即使用线程内唯一 ServiceProvider 对象</param>
+        public static T GetRequiredService<T>(bool useGlobalServiceProvider = false)
+        {
+            return GetIServiceProvider(useGlobalServiceProvider).GetRequiredService<T>();
         }
     }
 }
