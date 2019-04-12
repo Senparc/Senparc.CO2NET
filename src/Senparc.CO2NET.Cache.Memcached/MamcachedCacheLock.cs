18cba6479438f2237ad53501431b1f0b13137d5b
Jeffrey Su <zsu@senparc.com>
2019-04-13 00:41:51 +0800

    memcached 修改

--- src/Senparc.CO2NET.Cache.Memcached/MamcachedCacheLock.cs
+++ src/Senparc.CO2NET.Cache.Memcached/MamcachedCacheLock.cs
@@ -18,6 +18,7 @@
 
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Enyim.Caching.Memcached;
 using Senparc.CO2NET.Cache;
 using Senparc.CO2NET.Trace;
@@ -40,6 +41,8 @@ namespace Senparc.CO2NET.Cache.Memcached
         {
             return string.Format("{0}:{1}", "Lock", resourceName);
         }
+
+        #region 同步方法
 
         private bool RetryLock(string resourceName, int retryCount, TimeSpan retryDelay, Func<bool> action)
         {
@@ -103,5 +106,82 @@ namespace Senparc.CO2NET.Cache.Memcached
             var key = _mamcachedStrategy.GetFinalKey(resourceName);
             _mamcachedStrategy.Cache.Remove(key);
         }
+
+        #endregion
+
+        #region 异步方法
+#if !NET35 && !NET40
+
+
+        private async Task<bool> RetryLockAsync(string resourceName, int retryCount, TimeSpan retryDelay, Func<Task<bool>> action)
+        {
+            int currentRetry = 0;
+            int maxRetryDelay = (int)retryDelay.TotalMilliseconds;
+            while (currentRetry++ < retryCount)
+            {
+                if (await action())
+                {
+                    return true;//取得锁
+                }
+                Thread.Sleep(_rnd.Next(maxRetryDelay));
+            }
+            return false;
+        }
+
+        public override async Task<bool> LockAsync(string resourceName)
+        {
+            return await LockAsync(resourceName, 9999, new TimeSpan(0, 0, 0, 0, 20));
+        }
+
+        public override async Task<bool> LockAsync(string resourceName, int retryCount, TimeSpan retryDelay)
+        {
+            var key = _mamcachedStrategy.GetFinalKey(resourceName);
+            var successfull = await RetryLockAsync(key, retryCount, retryDelay, async () =>
+             {
+                 try
+                 {
+                     var ttl = base.GetTotalTtl(retryCount, retryDelay);
+#if NET45
+                    if (_mamcachedStrategy.Cache.Store(StoreMode.Add, key, new object(), TimeSpan.FromMilliseconds(ttl)))
+#else
+                    if (await _mamcachedStrategy.Cache.StoreAsync(StoreMode.Add, key, new object(), TimeSpan.FromMilliseconds(ttl)))
+#endif
+                    {
+                         return true;//取得锁 
+                    }
+                     else
+                     {
+                         return false;//已被别人锁住，没有取得锁
+                    }
+
+                    //if (_mamcachedStrategy._cache.Get(key) != null)
+                    //{
+                    //    return false;//已被别人锁住，没有取得锁
+                    //}
+                    //else
+                    //{
+                    //    _mamcachedStrategy._cache.Store(StoreMode.set, key, new object(), new TimeSpan(0, 0, 10));//创建锁
+                    //    return true;//取得锁
+                    //}
+                }
+                 catch (Exception ex)
+                 {
+                     SenparcTrace.Log("Memcached同步锁发生异常：" + ex.Message);
+                     return false;
+                 }
+             }
+              );
+            return successfull;
+        }
+
+        public override async Task UnLockAsync(string resourceName)
+        {
+            var key = _mamcachedStrategy.GetFinalKey(resourceName);
+            await _mamcachedStrategy.Cache.RemoveAsync(key);
+        }
+
+
+#endif
+        #endregion
     }
 }
