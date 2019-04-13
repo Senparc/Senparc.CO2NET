d7a7c1d03cbd4fe524f56bce758dd85ade6c3d5c
Jeffrey Su <zsu@senparc.com>
2019-04-13 21:48:56 +0800

    完成单元测试中Redis动态配置

--- src/Senparc.CO2NET.Tests/BaseTest.cs
+++ src/Senparc.CO2NET.Tests/BaseTest.cs
@@ -50,6 +50,30 @@ namespace Senparc.CO2NET.Tests
 
             registerService = RegisterService.Start(mockEnv.Object, _senparcSetting)
                 .UseSenparcGlobal(autoScanExtensionCacheStrategies);
+
+            //配置全局使用Redis缓存（按需，独立）
+            var redisConfigurationStr = _senparcSetting.Cache_Redis_Configuration;
+            var useRedis = !string.IsNullOrEmpty(redisConfigurationStr) && redisConfigurationStr != "#{Cache_Redis_Configuration}#"/*默认值，不启用*/;
+            if (useRedis)//这里为了方便不同环境的开发者进行配置，做成了判断的方式，实际开发环境一般是确定的，这里的if条件可以忽略
+            {
+                /* 说明：
+                 * 1、Redis 的连接字符串信息会从 Config.SenparcSetting.Cache_Redis_Configuration 自动获取并注册，如不需要修改，下方方法可以忽略
+                /* 2、如需手动修改，可以通过下方 SetConfigurationOption 方法手动设置 Redis 链接信息（仅修改配置，不立即启用）
+                 */
+                Senparc.CO2NET.Cache.Redis.Register.SetConfigurationOption(redisConfigurationStr);
+                Console.WriteLine("完成 Redis 设置");
+
+
+                //以下会立即将全局缓存设置为 Redis
+                Senparc.CO2NET.Cache.Redis.Register.UseKeyValueRedisNow();//键值对缓存策略（推荐）
+                Console.WriteLine("启用 Redis UseKeyValue 策略");
+
+                //Senparc.CO2NET.Cache.Redis.Register.UseHashRedisNow();//HashSet储存格式的缓存策略
+
+                //也可以通过以下方式自定义当前需要启用的缓存策略
+                //CacheStrategyFactory.RegisterObjectCacheStrategy(() => RedisObjectCacheStrategy.Instance);//键值对
+                //CacheStrategyFactory.RegisterObjectCacheStrategy(() => RedisHashSetObjectCacheStrategy.Instance);//HashSet
+            }
         }
     }
 }
