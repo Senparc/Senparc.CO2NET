f4eba404625812cc2f84910c80a556fd1e490ad0
Jeffrey Su <zsu@senparc.com>
2019-06-03 19:42:14 +0800

    添加 PostGetJsonStreamTest 单元测试

--- src/Senparc.CO2NET.Tests/Utilities/HttpUtility/PostTests.cs
+++ src/Senparc.CO2NET.Tests/Utilities/HttpUtility/PostTests.cs
@@ -27,6 +27,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Senparc.CO2NET.Tests;
+using System.Xml.Linq;
 
 namespace Senparc.CO2NET.HttpUtility.Tests
 {
@@ -92,6 +93,34 @@ namespace Senparc.CO2NET.HttpUtility.Tests
             catch (Exception ex)
             {
                 Assert.Fail();
+            }
+        }
+
+
+        [TestMethod]
+        public void PostGetJsonStreamTest()
+        {
+            //return;//已经通过，但需要连接远程测试，太耗时，常规测试时暂时忽略。
+            var url = "http://127.0.0.1:8099/weixin?timestamp=1559561525&nonce=05dde8fcd38e46fea0b7af7517831c5f&echostr=251ee1f4c800429282c824f46dd0f47b&signature=402a11f9b8537b08239607af0a789400796c5861";
+            try
+            {
+                var doc = new XDocument();
+                doc.Add(new XElement("xml"));
+                doc.Root.Add(new XElement("ToUserName", "Senparc"));
+                doc.Root.Add(new XElement("FromUserName", "NeuChar"));
+                doc.Root.Add(new XElement("CreateTime", Senparc.CO2NET.Helpers.DateTimeHelper.GetUnixDateTime(DateTime.Now).ToString()));
+                doc.Root.Add(new XElement("MsgType", "NeuChar"));
+                doc.Root.Add(new XElement("MsgId", DateTime.Now.Ticks.ToString()));
+                doc.Root.Add(new XElement("NeuCharMessageType", "GetConfig"));//设置类型
+
+
+                WxJsonResult resultFail = Post.PostGetJson<WxJsonResult>(url, cookieContainer: null, formData: null, encoding: null);
+                Assert.Fail();//上一步就应该已经抛出异常
+            }
+            catch (Exception ex)
+            {
+                //实际返回的信息（错误信息）
+                Console.WriteLine(ex.Message);
             }
         }
 
