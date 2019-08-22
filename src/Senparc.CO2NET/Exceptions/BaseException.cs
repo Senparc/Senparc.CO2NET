5e825a947544b396ffef97a0f9e0ee5a892c28dd
苏震巍 <zsu@senparc.com>
2019-08-22 12:35:42 +0800

    v0.8.9 正式发布

--- src/Senparc.CO2NET/Exceptions/BaseException.cs
+++ src/Senparc.CO2NET/Exceptions/BaseException.cs
@@ -1,5 +1,5 @@
 ﻿/*----------------------------------------------------------------
-    Copyright(C) 2018 Senparc
+    Copyright(C) 2019 Senparc
 
     文件名：BaseException.cs
     文件功能描述：异常基类
--- src/Senparc.CO2NET/Exceptions/FormFileDataException.cs
+++ src/Senparc.CO2NET/Exceptions/FormFileDataException.cs
@@ -1,5 +1,5 @@
 ﻿/*----------------------------------------------------------------
-    Copyright(C) 2018 Senparc
+    Copyright(C) 2019 Senparc
 
     文件名：FormFileData.cs
     文件功能描述：FormFileData 的异常
