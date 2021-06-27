9cca9bef0cfd2b59ff6f7c9f183dac0890b0e5cb
JeffreySu <www.jeffrey.su@gmail.com>
2021-06-27 21:31:08 +0800

    v0.5 添加 WebApi 相关方法

---
+++ src/Senparc.CO2NET.AspNet/WebApi/ApiBindAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.CO2NET.AspNet.WebApi
+{
+    /// <summary>
+    /// 自动绑定属性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class ApiBindAttribute : Attribute
+    {
+    }
+}
