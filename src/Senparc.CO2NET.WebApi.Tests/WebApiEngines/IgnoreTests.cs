ca2ca4b7d1fc297e27a2d83adfdc395de69da645
苏震巍 <zsu@senparc.com>
2023-06-14 13:55:19 +0800

    WebApi   v1.4 使用 [ApiBind(Ignore = false)]，添加完善对整个类的 API 生成忽略忽略

---
+++ src/Senparc.CO2NET.WebApi.Tests/WebApiEngines/IgnoreTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Senparc.CO2NET.ApiBind;
+using Senparc.NeuChar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senparc.CO2NET.WebApi.Tests.WebApiEngines
+{
+
+    [ApiBind(Ignore = true)]
+    public static class ApiBindTestClass_Ignore
+    {
+        public static void MethodForTest(string accessTokenOrApi, string p1, string p2)
+        {
+            Console.WriteLine($"accessTokenOrApi:{accessTokenOrApi} , p1:{p1} , p2 {p2}");
+        }
+    }
+
+    [ApiBind(Ignore = false)]
+    public static class ApiBindTestClass_Allow
+    {
+        public static void MethodForTest(string accessTokenOrApi, string p1, string p2)
+        {
+            Console.WriteLine($"accessTokenOrApi:{accessTokenOrApi} , p1:{p1} , p2 {p2}");
+        }
+    }
+
+    [TestClass]
+    public class IgnoreTests : BaseTest
+    {
+        public IgnoreTests() : base(true)
+        {
+
+        }
+
+        [TestMethod]
+        public void IgnoreApiBindTest()
+        {
+            var apiData = ApiBindInfoCollection.Instance;
+            apiData.Keys.ToList().ForEach(key => { Console.WriteLine(key); });
+
+            Assert.IsFalse(apiData.Keys.Any(z => z.Contains(nameof(ApiBindTestClass_Ignore))));
+            Assert.IsTrue(apiData.Keys.Any(z => z.Contains(nameof(ApiBindTestClass_Allow))));
+        }
+    }
+}
