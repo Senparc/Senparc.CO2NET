389fb71e6bb634040999d26ba89da169d4f1ff33
苏震巍 <zsu@senparc.com>
2024-02-09 01:14:00 +0800

    v2.4.0.1 修正 http 请求的超时时间设置问题

--- src/Senparc.CO2NET/Utilities/HttpUtility/HttpPost/RequestUtility.Post.cs
+++ src/Senparc.CO2NET/Utilities/HttpUtility/HttpPost/RequestUtility.Post.cs
@@ -310,10 +310,11 @@ namespace Senparc.CO2NET.HttpUtility
             senparcHttpClient.SetCookie(new Uri(url), cookieContainer);//设置Cookie
 
             HttpClient client = senparcHttpClient.Client;
+            client.Timeout = TimeSpan.FromMilliseconds(timeOut);
             HttpContent hc = null;
             HttpClientHeader(client, refererUrl, useAjax, headerAddition, timeOut);
 
-        #region 处理Form表单文件上传
+            #region 处理Form表单文件上传
 
             var formUploadFile = fileDictionary != null && fileDictionary.Count > 0;//是否用Form上传文件
             if (formUploadFile)
@@ -414,7 +415,7 @@ namespace Senparc.CO2NET.HttpUtility
             }
 
             //HttpContentHeader(hc, timeOut);
-        #endregion
+            #endregion
 
             if (!string.IsNullOrEmpty(refererUrl))
             {
@@ -909,7 +910,7 @@ namespace Senparc.CO2NET.HttpUtility
             #endregion
 
             var retString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-            
+
             response.Dispose();
 
             return retString;
